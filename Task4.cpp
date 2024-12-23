3a1eac87cc40faed86fff996cbd3229e40e1cd44
Wasif Ali <155293921+wasifali2004@users.noreply.github.com>
2024-12-23 09:17:59 +0500

    Add files via upload

--- Task4.cpp
+++ Task4.cpp
@@ -1,19 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    vector<int> v;
-    for(int i=0; i<5; ++i) {
-        cin >>v[i];
+class Stack{
+    private:
+    int arr[10];
+    int top;
+    public:
+    Stack() {
+        top = -1;
     }
-    int sum = 0 , maxi = INT_MIN;
-    for(int i=0; i<5; ++i) { //KARDANE ALGORITHM
-        sum += v[i];
-        maxi = max(sum ,maxi);
-        if(sum < 0) {
-            sum = 0;
+
+    void push(int x) {
+        if(top < 9) {
+            arr[++top] = x;
+            cout<<"Value " <<x <<" is inserted at position: "<<top << endl;
+        }
+        else {
+            cout<<"Stack is Full!" <<endl;
         }
     }
-    cout<<maxi;
 
+    int pop() {
+        if(top > -1) {
+            return arr[top--];
+        }
+        else {
+            cout<<"Stack is Empty!"<<endl;
+        }
+    }
+
+    void display() {
+    if(top == -1)  {
+        cout<<"Stack is empty!" <<endl;
+    }
+    else {
+        cout<<"Elements are: ";
+        for(int i = top; i>=0; --i) {
+            cout<<arr[i];
+        }
+    }
+}    
+};
+
+
+int main() {
+    Stack obj;
+    obj.push(10);
+    obj.push(30);
+    obj.push(20);
+    obj.push(40);
+    obj.push(50);
+    obj.push(60);
+    obj.push(70);
+    obj.push(80);
+    obj.push(90);
+    obj.push(100);
+    obj.display();
+    return 0;
 }
